dcf7a022e927a16abeea3bc1358e943eb0b73524
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-05-08 08:58:34 -0400

    separating backend

---
+++ secrets.js
@@ -0,0 +1,6 @@
+module.exports = {
+  CLIENT_ID: 'e3eedba3c2584d3db92f1c396df003bf',
+  CLIENT_SECRET: 'f00c61061c63407c838954aaab6d2e87',
+  accountSid: 'AC6b6ffad55e4477fb679bf3021a3a1bb3',
+  authToken: '907e5c298eb26c00b7885e6e9868c9a6'
+}
